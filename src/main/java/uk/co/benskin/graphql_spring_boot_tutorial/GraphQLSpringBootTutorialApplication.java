90127a464e751e33efbfe0180bf8d8a581248a58
Bic Ed <devan.oppenheimer@uniposta.de>
2019-07-26 08:59:53 +0200

    HOWTO:springboot 2 war@tomcat

--- src/main/java/uk/co/benskin/graphql_spring_boot_tutorial/GraphQLSpringBootTutorialApplication.java
+++ src/main/java/uk/co/benskin/graphql_spring_boot_tutorial/GraphQLSpringBootTutorialApplication.java
@@ -2,14 +2,27 @@ package uk.co.benskin.graphql_spring_boot_tutorial;
 import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc; 
 
 @SpringBootApplication
-public class GraphQLSpringBootTutorialApplication {
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {  "uk.co.benskin.graphql_spring_boot_tutorial.resolvers" } )
+public class GraphQLSpringBootTutorialApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 	SpringApplication app =new SpringApplication(GraphQLSpringBootTutorialApplication.class );
         app.setDefaultProperties(Collections
           .singletonMap("server.port", "8083"));
 	app.run(args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GraphQLSpringBootTutorialApplication.class);
     }
 }
