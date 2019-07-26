6f6cf59c47291ea17345d5c568c77df4ffebb097
Bic Ed <devan.oppenheimer@uniposta.de>
2019-07-26 11:13:25 +0200

    TEST: --

--- src/test/java/uk/co/benskin/graphql_spring_boot_tutorial/GraphiqlCsrfEnabledTest.java
+++ src/test/java/uk/co/benskin/graphql_spring_boot_tutorial/GraphiqlCsrfEnabledTest.java
@@ -1,35 +1,27 @@
 package uk.co.benskin.graphql_spring_boot_tutorial;
-
-  
+ 
 import static graphql.Assert.assertNotNull;
-import static org.junit.Assert.assertEquals;
-
-import java.io.IOException; 
-import org.junit.Before;
+import static org.junit.Assert.assertEquals;  
+import java.io.IOException;   
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired; 
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan; 
+import org.springframework.boot.test.context.SpringBootTest; 
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc; 
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner; 
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.oembedler.moon.graphiql.boot.GraphiQLAutoConfiguration;
 
 
 @ContextConfiguration(classes = {  GraphiQLAutoConfiguration.class, GraphQLSpringBootTutorialApplication.class})
-@ComponentScan("uk.co.benskin.graphql_spring_boot_tutorial.resolvers")
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GraphiqlCsrfEnabledTest { 
 	/** Logger */
-	private static final Logger LOG = LoggerFactory.getLogger(GraphiqlCsrfEnabledTest.class);
+	private static final Logger LOG = LoggerFactory.getLogger(GraphiqlCsrfEnabledTest.class); 
     @Autowired
     private GraphQLTestUtils graphQLTestUtils;
     
@@ -40,20 +32,10 @@ public class GraphiqlCsrfEnabledTest {
         assertNotNull(parsedResponse.get("data"));
         assertNotNull(parsedResponse.get("data").get("pets"));
         assertEquals("MAMMOTH", parsedResponse.get("data").get("pets").get(0).get("type").asText());
+        LOG.info("Done:{}",this.toString());
     }
       
   
- 
-	private MockMvc mockMvc;
-	@Autowired
-	private WebApplicationContext context;
-
-	@Before
-	public void init() {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-	}
-	
-	
 }
 
 
